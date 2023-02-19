593fa7f50311e6b67dc53d1865833f11fd4ec768
[mahapatrasonali] <[sonangr2000@gmail.com]>
2023-02-19 23:05:49 +0530

    Create BST

---
+++ BST.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class BinarySearchTree<T> where T : IComparable<T>
+    {
+        public T NodeData
+        {
+            get; set;
+        }
+        public BinarySearchTree(T nodedata)
+        {
+            this.NodeData = nodedata;
+            this.LeftTree = null;
+            this.RightTree = null;
+        }
+        public BinarySearchTree<T> LeftTree { get; set; }
+        public BinarySearchTree<T> RightTree { get; set; }
+        public void Insert(T data)
+        {
+
+            T cuurentNodeData = this.NodeData;
+            if ((cuurentNodeData.CompareTo(data)) > 0)
+            {
+                if (this.LeftTree == null)
+                {
+                    this.LeftTree = new BinarySearchTree<T>(data);
+                }
+                else
+                {
+                    this.LeftTree.Insert(data);
+                }
+            }
+            else
+            {
+                if (this.RightTree == null)
+                {
+                    this.RightTree = new BinarySearchTree<T>(data);
+                }
+                else
+                {
+                    this.RightTree.Insert(data);
+                }
+            }
+        }
+        public void Display()
+        {
+
+            if (this.LeftTree != null)
+            {
+                this.LeftTree.Display();
+            }
+            Console.WriteLine(this.NodeData.ToString());
+            if (this.RightTree != null)
+            {
+                this.RightTree.Display();
+            }
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace BinarySearchTree
+{
+    public class Program
+    {
+        public static void Main(string[] args) 
+        {
+            BinarySearchTree<int> bst = new BinarySearchTree<int>(56);
+            bst.Insert(30);
+            bst.Insert(70);
+            bst.Display();
+            Console.ReadLine();
+            Console.ReadLine();
+        }
+    }
+}
